917b85bd3e74c2a8fd8480105a782b04d56ec746
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-06-24 01:27:36 -0400

    added budget to database

---
+++ public/js/main.js
@@ -0,0 +1,24 @@
+const addBtn = document.querySelector('button');
+addBtn.addEventListener('click',addBudget);
+
+async function addBudget(){
+    const inputValue = Number(document.querySelector('input').value)
+
+    try{
+        const response =  await fetch('addBudget',{
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                budget:inputValue
+            })
+        })
+
+        const data = await response.json();
+        console.log(data);
+        location.reload();
+
+
+    }catch(err){
+       console.log(erro)
+    }
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const PORT = 3000;
+
+let db, 
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'budget-db';
+
+MongoClient.connect(dbConnectionString, {useUnifiedTopology: true})
+.then(client=>{
+    console.log(`connected to ${dbName} Database`)
+    db = client.db(dbName);
+})
+
+app.set('view engine','ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+
+app.post('/addBudget',(req,res)=>{
+    console.log(req.body)
+    db.collection('budget').insertOne({budget:req.body.budget})
+    .then(result=>{
+        console.log('Added budget');
+        res.json('Added budget')
+    })
+})
+app.listen(process.env.PORT || PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
