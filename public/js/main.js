10d9e50f995c4d94d1bf742752270f40a293898c
Jefrey Zavala <jefreyzavala1@gmail.com>
2022-07-06 22:00:07 -0400

    fixed functionality on budget app

--- public/js/main.js
+++ public/js/main.js
@@ -1,25 +1,23 @@
-const addBtn = document.querySelector('button');
-addBtn.addEventListener('click',addBudget);
+const addBtn = document.querySelector("button");
+addBtn.addEventListener("click", addBudget);
 
-async function addBudget(){
-    const inputValue = Number(document.querySelector('input').value)
+async function addBudget() {
+  const inputValue = Number(document.querySelector("input").value);
 
-    try{
-        const response =  await fetch('addBudget',{
-            method: 'post',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                budget:inputValue
-            })
-        })
+  try {
+    const response = await fetch("addBudget", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        budget: inputValue,
+      }),
+    });
 
-        const data = await response.json();
-        console.log(data);
-        document.querySelector('#div').classList.add("hide");
-        //location.reload();
-
-
-    }catch(err){
-       console.log(erro)
-    }
+    const data = await response.json();
+    console.log(data);
+    document.querySelector("#div").classList.add("hide");
+    //location.reload();
+  } catch (err) {
+    console.log(erro);
+  }
 }
